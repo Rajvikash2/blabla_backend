535c29a79ae1bb09594b6fc578bead7da013f78c
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2024-11-19 19:56:12 +0530

    connection to db enabled

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const postsRoutes = require('./routes/postRoutes')
 const app=express();
+const mongoose = require('mongoose');
 
 // middleware
 app.use(express.json())
@@ -13,7 +14,16 @@ app.use((req,res,next) =>{
 
 app.use('/api/post',postsRoutes)
 
+// Connect to DB
 
-app.listen(process.env.PORT,()=>{
-    console.log("server is running on port 4000");
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT,()=>{
+            console.log(" coonected to db & server is running on port",process.env.PORT);
+        })
+    })
+    .catch((error)=>{
+        console.log("Failed to connect",error);
+    })
+
+
